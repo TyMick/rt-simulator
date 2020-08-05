a8ac9cfd654bc1870a6de037ae761781dedb0e5c
Ty Mick <ty@tymick.me>
2020-08-05 12:08:35 -0400

    Tighten up introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -17,19 +17,8 @@ export default function Introduction() {
       </p>
 
       <p>
-        Entire websites, like{" "}
-        <a href="https://rt.live/">
-          R<sub>t</sub> Live
-        </a>{" "}
-        and{" "}
-        <a href="https://epiforecasts.io/covid/posts/global/">EpiForecasts</a>,
-        are dedicated to modeling and tracking <TeX>R_t</TeX> values in
-        different regions of the world, so (with exceptions, I&rsquo;m sure) it
-        isn&rsquo;t difficult to find a good <TeX>R_t</TeX> estimate for the
-        country or state you live in. But how much does the number matter?
-        It&rsquo;s fairly obvious that when <TeX>R_t = 2</TeX>, the virus will
-        spread pretty quickly, but what if <TeX>R_t = 1.1</TeX>? That
-        can&rsquo;t be too bad, right?
+        What may surprise you, though, is how sensitive that pivot point of{" "}
+        <TeX>R_t = 1.0</TeX> is. See for yourself:
       </p>
     </Container>
   );
