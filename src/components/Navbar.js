9d420b0dcc65ee02124c897d9c5adcbefbf15408
Ty Mick <ty@tymick.me>
2020-08-08 14:11:34 -0400

    Update navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar as RBNavbar, Container, Nav } from "react-bootstrap";
+import TeX from "@matejmazur/react-katex";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export default function Navbar() {
+  return (
+    <RBNavbar as="header" variant="dark" bg="dark">
+      <Container fluid="xl">
+        <RBNavbar.Brand>
+          <TeX>R_t</TeX> Simulator
+        </RBNavbar.Brand>
+        <Nav>
+          <Nav.Link
+            href="https://github.com/tywmick/rt-simulator"
+            title="tywmick/rt-simulator on GitHub"
+            className="pr-0"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </RBNavbar>
+  );
+}
