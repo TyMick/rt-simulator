692cac50bd342c9918d91cfc328e58f960cfd283
Ty Mick <ty@tymick.me>
2020-08-08 18:33:41 -0400

    Adjust headings on small screens

--- src/components/Headings.js
+++ src/components/Headings.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import useBreakpoints from "use-window-width-breakpoints";
-import Subtitle from "./Subtitle";
+import "./Headings.scss";
 
 export default function Headings() {
   const breakpoint = useBreakpoints();
 
   return (
     <Container fluid={breakpoint.up.lg} className="text-lg-center">
-      <h1>
+      <h1 className="long-heading">
         Why it&rsquo;s so important to keep COVID&rsquo;s
         {breakpoint.up.lg ? <br /> : " "}reproduction number below 1
       </h1>
-      <Subtitle>A lesson in exponential growth</Subtitle>
+      <div id="subtitle">A lesson in exponential growth</div>
     </Container>
   );
 }
