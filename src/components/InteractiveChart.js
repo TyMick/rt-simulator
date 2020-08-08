b821472354b6b218cdfef9d18a302518beae2b98
Ty Mick <ty@tymick.me>
2020-08-08 18:33:53 -0400

    Complete animation logic

--- src/components/InteractiveChart.js
+++ src/components/InteractiveChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useWindowWidthBreakpoints from "use-window-width-breakpoints";
 import { Container } from "react-bootstrap";
 import { VegaLite } from "react-vega";
@@ -13,8 +13,41 @@ export default function InteractiveChart({
   setInitialDailyInfections,
 }) {
   const bp = useWindowWidthBreakpoints();
+
+  const initialDomainMax = generateSimData(1.1, initialDailyInfections).pop()
+    .newInfections;
   const [rt, setRt] = useState(1.1);
+
   const [animating, setAnimating] = useState(true);
+  const [animationDirection, setAnimationDirection] = useState("down");
+  const [timeAtEnds, timeWhileMoving] = [3000, 50];
+  useEffect(() => {
+    if (animating) {
+      let timer;
+      if (rt === 0.9) {
+        timer = setTimeout(() => {
+          setAnimationDirection("up");
+          setRt(0.91);
+        }, timeAtEnds);
+      } else if (rt === 1.1) {
+        timer = setTimeout(() => {
+          setAnimationDirection("down");
+          setRt(1.09);
+        }, timeAtEnds);
+      } else {
+        const nextRt =
+          Math.round(
+            (animationDirection === "up" ? rt + 0.01 : rt - 0.01) * 100
+          ) / 100; // Float troubles
+        timer = setTimeout(() => {
+          setRt(nextRt);
+        }, timeWhileMoving);
+      }
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [rt, animating, animationDirection]);
 
   const successColor = "#28a745";
   const warningColor = "#ffc107";
@@ -22,13 +55,12 @@ export default function InteractiveChart({
   const rtColorScale = chroma
     .scale([successColor, warningColor, dangerColor])
     .domain([0.95, 1.05]);
-  const rtColor = (rt) =>
-    rt < 0.95 ? successColor : rt > 1.05 ? dangerColor : rtColorScale(rt).hex();
 
   const vegaSpec = {
     width: "container",
     height: "container",
     autosize: { contains: "padding" },
+    background: "transparent",
     mark: { type: "line" },
     data: { name: "spread" },
     encoding: {
@@ -39,7 +71,8 @@ export default function InteractiveChart({
         axis: {
           tickCount: "month",
           labelFontSize: 14,
-          labelAngle: bp.xs ? -50 : 0,
+          labelAngle: bp.xs ? -30 : 0,
+          labelOverlap: false,
         },
       },
       y: {
@@ -51,8 +84,9 @@ export default function InteractiveChart({
           titleFontWeight: 400,
           labelFontSize: 14,
         },
+        scale: { domainMax: rt > 1.1 ? null : initialDomainMax },
       },
-      color: { value: rtColor(rt) },
+      color: { value: rtColorScale(rt).hex() },
     },
   };
   const infectionSpreadSim = {
@@ -61,13 +95,17 @@ export default function InteractiveChart({
 
   return (
     <Container as="figure" fluid="xl" className="cap-width-lg">
-      <figcaption className={clsx("text-center mb-0", bp.xs ? "h5" : "h4")}>
-        {bp.xs && "Daily new infections when "}
+      <figcaption
+        className={clsx("text-center mb-n2", bp.xs ? "h5" : "h4")}
+        style={{ zIndex: 10000000000 }}
+      >
+        {bp.xs && <div className="h5 mb-0">Daily new infections when</div>}
         <TeX
-          math={`R_t = ${rt === 1 ? "1.0" : rt}`}
-          block={bp.up.sm}
-          style={{ color: rtColor(rt) }}
+          math={`R_t = ${rt}${rt.toString().length === 1 ? ".0" : ""}`}
+          style={{ color: rtColorScale(rt).hex() }}
         />
+        {/* Add an invisible 0 for spacing on multiples of 0.1 */}
+        {rt.toString().length !== 4 && <TeX math="0" className="invisible" />}
       </figcaption>
       <VegaLite spec={vegaSpec} data={infectionSpreadSim} actions={false} />
 
