666a3067662f14c510f28563697dfc48ba8a724c
Ty Mick <ty@tymick.me>
2020-08-08 14:19:20 -0400

    Fix navbar import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 import Headings from "./components/Headings";
 import Introduction from "./components/Introduction";
 
 export default function App() {
   return (
     <>
-      <Nav />
+      <Navbar />
       <Headings />
       <Introduction />
     </>
