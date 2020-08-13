e7e8c1bc305d66a9f871a8c0ec695419ea6045af
Ty Mick <ty@tymick.me>
2020-08-13 14:58:18 -0400

    Extract StatePicker

---
+++ src/components/StatePicker.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Form, Col } from "react-bootstrap";
+import TeX from "@matejmazur/react-katex";
+import useCovidData from "../hooks/useCovidData";
+
+export default function StatePicker({
+  setRt,
+  setInitialDailyInfections,
+  region,
+  setRegion,
+  setAnimating,
+  setUserHasChangedIDI,
+}) {
+  const { stateData } = useCovidData();
+
+  return (
+    <Form.Group as={Form.Row} controlId="statePicker">
+      <Form.Label column xs={12} sm="auto">
+        Pick a U.S. state to fill its current <TeX>R_t</TeX> estimate:
+      </Form.Label>
+      <Col xs={12} sm md="auto">
+        <Form.Control
+          as="select"
+          value={region}
+          custom
+          onChange={(e) => {
+            setAnimating(false);
+            setUserHasChangedIDI(true);
+
+            const region = e.target.value;
+            setRegion(region);
+            if (region) {
+              setRt(stateData[region].rtEstimate);
+              setInitialDailyInfections(stateData[region].newCases7DayAvg);
+            }
+          }}
+        >
+          <option></option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </Form.Control>
+      </Col>
+    </Form.Group>
+  );
+}
