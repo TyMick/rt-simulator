f743c96915f78406a08602975e1b68524d5ca798
Ty Mick <ty@tymick.me>
2020-08-18 21:13:02 -0400

    Rearrange chart caption a bit

--- components/chart-caption.js
+++ components/chart-caption.js
@@ -1,27 +1,25 @@
 import React from "react";
 import useWindowWidthBreakpoints from "use-window-width-breakpoints";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import TeX from "@matejmazur/react-katex";
 import clsx from "clsx";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export default function ChartCaption({ rt, rtLower80, rtUpper80, getRtColor }) {
   const bp = useWindowWidthBreakpoints();
 
   return (
-    <figcaption
-      className={clsx("text-center mb-n1", bp.xs ? "h5" : "h4")}
-    >
+    <figcaption className={clsx("text-center mb-n1", bp.xs ? "h5" : "h4")}>
       {bp.xs && <div className="h5 mb-0">Daily new infections when</div>}
 
       <TeX
         math={`R_t ${rtLower80 && rtUpper80 ? "\\approx" : "="} ${rt}${
-          rt.toString().length === 1 ? ".0" : ""
+          rt % 1 === 0 ? ".0" : ""
         }`}
         style={{ color: getRtColor(rt) }}
       />
       {/* Adding an invisible 0 for spacing on multiples of 0.1 when not using
       //* confidence intervals */}
-      {rt.toString().length !== 4 && rtLower80 === undefined && (
+      {rt % 0.1 === 0 && rtLower80 === undefined && (
         <TeX math="0" className="invisible" />
       )}
 
@@ -30,7 +28,7 @@ export default function ChartCaption({ rt, rtLower80, rtUpper80, getRtColor }) {
           <TeX math={`\\scriptsize \\text{80\\% }`} className="ml-3 ml-sm-4" />
           <OverlayTrigger
             overlay={
-              <Tooltip id="CI">
+              <Tooltip id="ci">
                 Confidence Interval
                 <br />
                 (the shaded part of the graph)
