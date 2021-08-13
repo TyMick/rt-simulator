53e9d2aca6116ca2d66432509a4832cf390f8e60
Ty Mick <ty@tymick.me>
2021-08-13 08:29:31 -0700

    Switch to useLayoutEffect for animation

--- hooks/use-animation.js
+++ hooks/use-animation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 export default function useAnimation(
   animating,
@@ -10,7 +10,7 @@ export default function useAnimation(
 
   const [direction, setDirection] = useState("up");
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (animating) {
       let timer;
       if (rt === 0.9 && direction === "down") {
